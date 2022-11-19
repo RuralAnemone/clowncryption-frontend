db25c40d74d332bb10ffca1511a78a2dbc0a8218
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-19 20:59:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var crypt = "encrypt";
+document.querySelector('form').onchange = () => {
+  crypt = document.querySelector('input[name = "method"]:checked').value;
+  
+  document.querySelector('#text-input').placeholder = `message to ${document.querySelector('input[name = "method"]:checked').value}`;
+  
+  document.querySelector('input[type = "submit"]').value = `${crypt.substring(0,1).toUpperCase()}${crypt.substring(1)}!`;
+
+  document.querySelector('#key-input').placeholder = `${crypt}ion key`;
+}
+
+function submit () {
+  var method = document.querySelector('input[name = "method"]:checked').value;
+  var key = document.querySelector('#key-input').value;
+  var iv = document.querySelector('input[name = "iv"]').value;
+  var message = document.querySelector('#text-input').value;
+  var salt = document.querySelector('input[name = "salt"]').value; // hehe pepper
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', )
+}
