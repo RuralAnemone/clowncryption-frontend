dc7942b416c9b9006f39c14a3de76758b30a7152
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-19 21:49:30 +0000

    packages

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+// variables and prototypes
+// ---------
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env['PORT'] || 42069;
 const ClownCryption = require('clowncryption');
-// as soon as it's on npm :)
+
+// https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+String.prototype.toHHMMSS = function() {
+  var sec_num = parseInt(this, 10);
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours < 10) { hours = "0" + hours; }
+  if (minutes < 10) { minutes = "0" + minutes; }
+  if (seconds < 10) { seconds = "0" + seconds; }
+  return hours + ':' + minutes + ':' + seconds;
+}
+
+// ---------
 
 // server
-app.use("/", express.static(path.join(__dirname, "/")));
+app.use("/", express.static(path.join(__dirname, 'public')));
 
 app.post("/crypt", (req, res) => {
   const usp = new URLSearchParams(req.query);
-  if (usp.get(method)=="encrypt") {
+  if (usp.get(method) == "encrypt") {
     res.send(ClownCryption.encrypt({
       message: usp.get(message),
       key: usp.get(key),
       iv: usp.get(iv)
     }))
-  } else if (usp.get(method)=="decrypt") {
+  } else if (usp.get(method) == "decrypt") {
     res.send(ClownCryption.decrypt({
       message: usp.get(message),
       key: usp.get(key),
@@ -28,4 +44,14 @@ app.post("/crypt", (req, res) => {
 app.use((req, res, next) => {
   res.status(404).sendFile('/home/runner/clowncryption-frontend/404.html')
 })
-app.listen(port, _=>console.log("online!"));
+app.listen(port, () => {
+  setInterval(() => {
+    console.clear();
+    console.log(`
+Uptime: ${process.uptime().toString().toHHMMSS()}
+
+online!
+listening on port: ${port}
+    `);
+  }, 1000);
+});
--- replit.nix
+++ replit.nix
@@ -1,9 +1,8 @@
 { pkgs }: {
 	deps = [
-      pkgs.vim
-      pkgs.nodejs-16_x
-      pkgs.nodePackages.typescript-language-server
-      pkgs.yarn
-      pkgs.replitPackages.jest
+		pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
 	];
 }
