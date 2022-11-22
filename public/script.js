9e589ec35ddd4b3987dfc6e95d84dc442d726b6f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-22 11:52:17 -0600

    peace of mind thank you stackoverflow answer #4213253

--- public/script.js
+++ public/script.js
@@ -2,15 +2,15 @@
 // --------------------------------------------------------------------
 
 var crypt = "encrypt";
-document.querySelector('#form').onchange = () => {
-  crypt = document.querySelector('input[name = "method"]:checked').value;
-  
-  document.querySelector('#text-input').placeholder = `message to ${document.querySelector('input[name = "method"]:checked').value}`;
-  
-  document.querySelector('input[type = "submit"]').value = `${crypt.substring(0,1).toUpperCase()}${crypt.substring(1)}!`;
-
-  document.querySelector('#key-input').placeholder = `${crypt}ion key`;
-}
+Array.from(document.querySelectorAll('input')).forEach(e => {
+  e.onchange = () => {
+    crypt = document.querySelector('input[name = "method"]:checked').value;
+    document.querySelector('#text-input').placeholder = `message to ${document.querySelector('input[name = "method"]:checked').value}`;
+    document.querySelector('input[type = "submit"]').value = `${crypt.substring(0,1).toUpperCase()}${crypt.substring(1)}!`;
+    document.querySelector('#key-input').placeholder = `${crypt}ion key`;
+    console.log(crypt);
+  }
+})
 
 function submit () {
   var method = document.querySelector('input[name = "method"]:checked').value;
@@ -18,15 +18,9 @@ function submit () {
   var iv = document.querySelector('input[name = "iv"]').value;
   var message = document.querySelector('#text-input').value;
   var salt = document.querySelector('input[name = "salt"]').value; // hehe pepper
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", reqListener);
-  // xhr.open('GET', `./crypt?method=${crypt}&message=${message}&key=${key}&iv=${iv}`);
-  xhr.open('GET', `./uptime?${crypt == "encrypt" ? "hhmmss" : ""}`);
-  xhr.send();
-}
-
-function reqListener() {
-  document.querySelector('#result').innerHTML = this.responseText;
+  fetch(`./crypt?method=${crypt}&message=${message}&key=${key}&iv=${iv}`).then(res => {res.text().then(text => {
+    document.querySelector('#result').innerHTML = text;
+  })})
 }
 
 // --------------------------------------------------------------------
