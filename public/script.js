d51940c9c957cfe405a11e793e9f158c57ebed81
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-22 15:02:30 -0600

    fix various things I think I hope I wonder

--- public/script.js
+++ public/script.js
@@ -4,13 +4,19 @@
 var crypt = "encrypt";
 var charsetType = "hexLiteral";
 
-document.querySelector('#form').onchange = () => {
-  crypt = document.querySelector('input[name = "method"]:checked').value;
+Array.from(document.querySelectorAll('input[name="method"]')).forEach(_=>_.onchange = () => {
+  crypt = document.querySelector('input[name="method"]:checked').value;
   document.querySelector('#text-input').placeholder = `message to ${document.querySelector('input[name = "method"]:checked').value}`;
-  document.querySelector('input[type = "submit"]').value = `${crypt.substring(0,1).toUpperCase()}${crypt.substring(1)}!`;
+  document.querySelector('input[type="submit"]').value = `${crypt.substring(0,1).toUpperCase()}${crypt.substring(1)}!`;
   document.querySelector('#key-input').placeholder = `${crypt}ion key`;
   console.log(crypt);
-}
+})
+
+Array.from(document.querySelectorAll('input[name="charset"]')).forEach(_=>_.onchange = () => {
+  charsetType = document.querySelector('input[name="charset"]:checked').value;
+  console.log(charsetType)
+  editor.set(defaultCharset())
+})
 
 function submit () {
   const method = document.querySelector('input[name = "method"]:checked').value;
@@ -18,7 +24,7 @@ function submit () {
   const iv = document.querySelector('input[name = "iv"]').value;
   const message = document.querySelector('#text-input').value;
   const salt = document.querySelector('input[name = "salt"]').value; // hehe pepper
-  const charset = updatedJson;
+  const charset = editor.get();
   fetch(`./crypt?method=${crypt}&message=${message}&key=${key}&iv=${iv}`).then(res => {res.text().then(text => {
     document.querySelector('#result').innerHTML = text;
   })})
