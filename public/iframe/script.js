ae34c39c01dee0e1b100a28d9570ec7e807b6b2e
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-12-02 16:31:55 -0600

    supports newline now

--- public/iframe/script.js
+++ public/iframe/script.js
@@ -1,6 +1,6 @@
 const hash = location.hash.substring(1); // effectively remove the # from the hash so it's only text
 const cp = new URLSearchParams(location.search).has("cp")
-const html = cp?`<textarea autofocus>${decodeURIComponent(hash)}</textarea>`:decodeURIComponent(hash)
+const html = cp?`<textarea autofocus>${atob(decodeURIComponent(hash))}</textarea>`:decodeURIComponent(hash)
 
 document.body.onload=_=>{
     document.write(html)
