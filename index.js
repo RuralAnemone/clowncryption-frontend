64f44379912e4eed999af620c9f6be9abca962bb
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-12-08 11:00:49 -0600

    oh yeah and this

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env['PORT'] || 42069;
-const ClownCryption = require('clowncryption');
+const ClownCryption = require('clowncryption').default;
 
 // https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
 String.prototype.toHHMMSS = function() {
